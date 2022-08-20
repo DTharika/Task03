3f8c9bf0b731d399fdff75d2ed234ec57bde4590
DanujiTharika <65587532+DanujiTharika@users.noreply.github.com>
2022-08-20 16:05:07 +0530

    Add project files.

---
+++ Task03/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task03.Models;
+
+namespace Task03.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ Task03/Models/Add ContactRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Task03.Models
+{
+    public class Add_ContactRequest
+    {
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public long Phone { get; set; }
+
+        public string Address { get; set; }
+    }
+}
